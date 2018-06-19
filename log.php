a5251703b4ebf21f9baca3e73d1055c9475fc92a
Unknown <andriwicaksonost@gmail.com>
2018-06-19 12:24:45 +0700

    Command Log

---
+++ log.php
@@ -0,0 +1,17 @@
+<?php 
+//Command Logs
+
+/*
+php artisan migrate
+php artisan migrate:fresh --seed
+php artisan make:migration create_posts_table --create=posts
+php artisan make:seeder PostsTableSeeder
+php artisan make:model Post -m
+composer require laravelcollective/html
+php artisan make:controller
+php artisan make:auth
+composer require "santigarcor/laratrust:5.0.*"
+php artisan vendor:publish --tag="laratrust"
+
+
+*/
